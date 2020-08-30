8ffa1409e8fbe1e12b8b3bd87ce1102370070ece
YannChye <yannying.chye@monash.edu>
2020-08-30 13:06:54 +1000

    plotly homework

---
+++ static/js/app.js
@@ -0,0 +1,95 @@
+function init() {
+    var dropDown=d3.select("#selDataset");
+    //populate dropdown options
+    d3.json("samples.json").then(data => {
+        var subIDs=data.names;
+        subIDs.forEach((subID) => {
+            dropDown.append("option").text(subID).property("value",subID);
+        })
+    })
+    //initial metadata-using subject 940
+    getMetadata("940");
+    //initial otu data
+    d3.json("samples.json").then(data => {
+        var selected=data.samples.filter(sample => sample.id==="940");
+        var otuIds=selected[0].otu_ids;
+        var otuLabels=selected[0].otu_labels;
+        var otuVals=selected[0].sample_values;
+        //initial bar chart
+        var traceBar={
+            type:"bar",
+            y:otuIds.slice(0,10).reverse().map(id=>`OTU ${id}`),
+            x:otuVals.slice(0,10).reverse(),
+            text:otuLabels.slice(0,10).reverse(),
+            orientation:"h"
+        };
+        var dataBar=[traceBar];
+        Plotly.newPlot("bar",dataBar);
+        //initial bubble chart
+        var traceBubble={
+            type:"bubble",
+            x:otuIds,
+            y:otuVals,
+            text:otuLabels,
+            mode:"markers",
+            marker:{
+                color:otuIds,
+                colorscale:"Viridis",
+                opacity:[0.8],
+                size:otuVals,
+                sizemode:"area",
+                sizeref:0.2
+            }
+        };
+        var dataBubble=[traceBubble];
+        var layoutBubble={
+            showlegend:false,
+            xaxis:{title:"OTU ID"},
+            yaxis:{title:"sample values"}
+        };
+        Plotly.newPlot("bubble",dataBubble,layoutBubble);
+    })
+}
+//update subject otus based on dropdown selection
+function updateSubject(subject) {
+    d3.json("samples.json").then(data => {
+        var selected=data.samples.filter(sample => sample.id===subject);
+        var otuIds=selected[0].otu_ids;
+        var otuLabels=selected[0].otu_labels;
+        var otuVals=selected[0].sample_values;
+        //update bar chart
+        Plotly.restyle("bar", "x", [otuVals.slice(0,10).reverse()]);
+        Plotly.restyle("bar", "y", [otuIds.slice(0,10).reverse().map(id=>`OTU ${id}`)]);
+        Plotly.restyle("bar", "text", [otuLabels.slice(0,10).reverse()]);
+        //update bubble chart
+        var marker={
+            color:otuIds,
+            colorscale:"Viridis",
+            opacity:[0.8],
+            size:otuVals,
+            sizemode:"area",
+            sizeref:0.2}
+        Plotly.restyle("bubble", "x", [otuIds]);
+        Plotly.restyle("bubble", "y", [otuVals]);
+        Plotly.restyle("bubble", "text", [otuLabels]);
+        Plotly.restyle("bubble", "marker", [marker]);
+    })
+}
+//get and populate subject metadata
+function getMetadata(subject) {
+    d3.json("samples.json").then(data => {
+        var metadata=data.metadata;
+        var selected=metadata.filter(sample => sample.id===parseInt(subject));
+        d3.select("#sample-metadata").selectAll("*").remove();
+        Object.entries(selected[0]).forEach(([key, value]) =>
+            {if (value.length>15) {value=value.replace("/","/\n")};
+            d3.select("#sample-metadata").append("li").style("list-style-type","none").text(`${key}: ${value}`)}
+        ) 
+    })
+}
+function optionChanged(subject) {
+    getMetadata(subject);
+    updateSubject(subject);
+    changeGauge(subject);
+}
+init();
---
+++ static/js/bonus.js
@@ -0,0 +1,69 @@
+//initial gauge chart
+//adapted from https://com2m.de/blog/technology/gauge-charts-with-plotly/
+function init() {
+    d3.json("samples.json").then(data => {
+        // getting wash freq from subject 940
+        var wFreq=data.metadata.filter(sample => sample.id===940)[0].wfreq;
+        // creating pointer
+        var path=needlePath(wFreq);
+        // creating gauge
+        var dataGauge = [{ 
+                type:"scatter",
+                x:[0],
+                y:[0],
+                marker:{size:14,color:'850000'},
+                showlegend:false},
+                {values: [1,1,1,1,1,1,1,1,1,9],
+                rotation: 90,
+                text: ["8-9","7-8","6-7","5-6","4-5","3-4","2-3","1-2","0-1"," "],
+                textinfo: 'text',
+                textposition:'inside',
+                hoverinfo:"none",
+                marker: {colors:['rgb(103,165,123)','rgb(120,179,130)','rgb(137,192,137)',
+                    'rgb(177,212,158)','rgb(213,229,158)','rgb(230,232,179)',
+                    'rgb(240,237,204)','rgb(245,241,229)','rgb(248,246,242)',
+                     'rgba(0,0,0,0)']},
+                hole:.5,
+                type: 'pie',
+                showlegend: false
+            }];
+        var layoutGauge = {
+            title: { text: "<b>Belly Button Washing Frequency</b><br>Scrubs per week" }, 
+            shapes:[{
+                type:"path",
+                path:path,
+                fillcolor:"850000",
+                line: {color:"850000"}
+            }],
+            xaxis: {zeroline:false,showticklabels:false,showgrid:false,range:[-1, 1]},
+            yaxis: {zeroline:false,showticklabels:false,showgrid:false,range:[-1, 1]}
+        };
+        Plotly.newPlot("gauge",dataGauge,layoutGauge);
+    })
+}
+//update gauge needle when subject dropdown changes
+function changeGauge(subject) {
+    d3.json("samples.json").then(data => {
+        var wFreq=data.metadata.filter(sample => sample.id===parseInt(subject))[0].wfreq;
+        var path=needlePath(wFreq);
+        var layoutUpdate = {type:"path",
+                path:path,
+                fillcolor:"850000",
+                line: {color:"850000"}};
+        Plotly.relayout("gauge", "shapes", [layoutUpdate])
+       })};
+//creating needle path as per https://com2m.de/blog/technology/gauge-charts-with-plotly/
+function needlePath(value) {
+    var degrees = (9-value)/9*180;
+    var x = 0.5 * Math.cos(degrees * Math.PI / 180);
+    var y = 0.5 * Math.sin(degrees * Math.PI / 180);
+    var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+    var mainPath=path1;
+    var pathX=String(x);
+    var space=" ";
+    var pathY=String(y);
+    var pathEnd=" Z";
+    var path=mainPath.concat(pathX,space,pathY,pathEnd);
+    return path
+}
+init()
